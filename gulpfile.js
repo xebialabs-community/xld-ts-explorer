8dcf9dac8799abb0697929637d05f83f6cd9a5ab
Bogdan Nechyporenko <bnechyporenko@xebialabs.com>
2018-03-16 11:55:43 +0100

    automated building process by running only 1 command

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var pluginDest = 'src/main/resources/web/xld-ts-explorer';
-
 var libDist = 'node_modules/type-system-explorer/dist/**';
+
+require('gulp-release-it')(gulp);
 
 gulp.task('copy', function () {
     gulp
